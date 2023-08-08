cd2f6cc950a6dbb59fd4deef9c864d494bc8e08f
Godha Karthik Reddy <paulatreides@duck.com>
2023-08-08 16:29:15 +0530

    Done with Semester

--- assets/java/Eclipse.java
+++
@@ -1,8 +0,0 @@
-public class Eclipse {
-    public static void main(String[] args) {
-        System.out.println("\n Prog. is showing even no"); for(int i=2;i<=20;i++)
-        {
-            if(i%2==0) {
-                System.out.print("\n "+i); }
-        } }
-}
